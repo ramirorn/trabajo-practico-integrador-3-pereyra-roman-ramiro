07e02fd33d9e4416d000337d330d6a0f0710e324
Ramiro <ramiroroman306@gmail.com>
2025-11-13 21:03:53 -0300

    Cambio en el loading

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -3,7 +3,7 @@ export const Loading = () => {
     <div className="flex flex-col items-center justify-center p-12">
       <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
       <p className="mt-4 text-lg font-semibold text-gray-700">
-        Cargando personaje...
+        Cargando...
       </p>
     </div>
   );
